2b8a330fb6c40ccbb259e716b6af41417122eaf4
luciequant <156855912+luciequant@users.noreply.github.com>
2024-05-14 20:57:16 +0200

    Added phonetics

--- src/Results.js
+++ src/Results.js
@@ -7,15 +7,17 @@ export default function Results(props) {
   if (props.data) {
     return (
       <div>
-        <h2>{props.data.word}</h2>
-
-        {props.data.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning data={meaning} />
-            </div>
-          );
-        })}
+        <div className="Results">
+          <h2>{props.data.word}</h2>
+          <p>/{props.data.phonetic}/</p>
+          {props.data.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning data={meaning} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
