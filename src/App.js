d18ba1aab8c60f8e31860727f04dc6c72502f582
luciequant <156855912+luciequant@users.noreply.github.com>
2024-05-14 16:08:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center">Dictionary</h1>
+        </header>
+        <footer className="text-center">Coded by Lucie Quantová</footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
